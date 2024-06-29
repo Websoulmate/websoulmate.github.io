a11b48ef2bec83caf09c3e81584c8069ee0740d7
Pasang Tamang <110292256+Websoulmate@users.noreply.github.com>
2024-06-29 05:33:18 +0300

    change hamburger style and added navbar background

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const toggleButton = document.getElementsByClassName('hamburger')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click',() => {
+	toggleButton.classList.toggle('active')
   navbarLinks.classList.toggle('active')
 })
