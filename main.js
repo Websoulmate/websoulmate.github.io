429f4762f9336eff3f380fa43184b4e12165e088
Pasang Tamang <110292256+Websoulmate@users.noreply.github.com>
2024-07-23 03:30:44 +0300

    change navbar styles

--- main.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.getElementsByClassName('hamburger')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click',() => {
-	toggleButton.classList.toggle('active')
-  navbarLinks.classList.toggle('active')
-})
